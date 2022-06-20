1574b98df6b69b9a9418f33393e5d0c88a936a5a
Cakegod <cakemancerr@gmail.com>
2022-06-20 20:56:30 +0200

    added most of the gameboard logic

--- src/modules/gameboard.js
+++ src/modules/gameboard.js
@@ -6,30 +6,79 @@ export const gameBoard = () => {
     boardArray[i] = new Array(10).fill("");
   }
 
+  const carrier = ship({ length: 1, name: "carrier" });
+  const battleship = ship({ length: 1, name: "battleship" });
+  const destroyer = ship({ length: 1, name: "destroyer" });
+  const submarine = ship({ length: 1, name: "submarine" });
+  const patrolBoat = ship({ length: 1, name: "patrol boat" });
 
-  const carrier = { length: 5, name: "carrier" };
-  const battleship = { length: 4, name: "battleship" };
-  const destroyer = { length: 3, name: "destroyer" };
-  const submarine = { length: 3, name: "submarine" };
-  const patrolBoat = { length: 2, name: "patrol boat" };
+  const isShipFit = (length, direction, x, y) => {
+    switch (direction) {
+      case "horizontal":
+        return x <= 10 - length;
+      case "vertical":
+        return y <= 10 - length;
+      default: // nothing
+    }
+  };
+
+  const isPlaceEmpty = (length, direction, x, y) => {
+    let empty = true;
+    if (direction === "horizontal") {
+      for (let i = x; i <= x + length - 1; i += 1) {
+        if (boardArray[i][y] !== "") {
+          empty = false;
+        }
+      }
+    } else if (direction === "vertical") {
+      for (let i = y; i <= y + length - 1; i += 1) {
+        if (boardArray[x][i] !== "") {
+          empty = false;
+        }
+      }
+    }
+    return empty;
+  };
 
   const placeShip = (shipName, direction, x, y) => {
-    if (direction === "horizontal" && x <= 10 - shipName.length) {
+    if (!isShipFit(shipName.length, direction, x, y)) {
+      return "ship does not fit";
+    }
+    if (!isPlaceEmpty(shipName.length, direction, x, y)) {
+      return "place is not empty";
+    }
+    if (direction === "horizontal") {
       for (let i = x; i <= x + shipName.length - 1; i += 1) {
         boardArray[i][y] = ship(shipName);
       }
-    } else if (direction === "vertical" && y <= 10 - shipName.length) {
+    } else if (direction === "vertical") {
       for (let i = y; i <= y + shipName.length - 1; i += 1) {
         boardArray[x][i] = ship(shipName);
       }
     }
+    return "placed";
+  };
+
+  const areShipSunk = () => {
+    if (
+      carrier.isSunk() &&
+      battleship.isSunk() &&
+      destroyer.isSunk() &&
+      submarine.isSunk() &&
+      patrolBoat.isSunk()
+    ) {
+      return true;
+    }
+    return false;
   };
 
   const missedHits = [];
 
   const receiveAttack = (x, y) => {
     if (boardArray[x][y] !== "") {
-      return boardArray[x][y].hit();
+      boardArray[x][y].hit();
+      areShipSunk();
+      return "hit";
     }
     return missedHits.push([x, y]);
   };
@@ -50,6 +99,7 @@ export const gameBoard = () => {
     destroyer,
     submarine,
     patrolBoat,
+    areShipSunk,
   };
 };
 
--- src/tests/battleship.test.js
+++ src/tests/battleship.test.js
@@ -1,37 +1,39 @@
 /* eslint-disable no-undef */
-import { ship } from '../modules/ship';
-import { gameBoard } from '../modules/gameboard';
+import { ship } from "../modules/ship";
+import { gameBoard } from "../modules/gameboard";
 
-const shipName = { length: 3, name: 'testerino' };
+const shipName = { length: 3, name: "testerino" };
 const testShip = ship(shipName);
 
-describe('SHIP', () => {
-	test('ship is not sunk', () => {
-		testShip.hit();
-		expect(testShip.isSunk()).toBe(false);
-	});
-	test('ship is sunk', () => {
-		testShip.hit();
-		testShip.hit();
-		testShip.hit();
-		expect(testShip.isSunk()).toBe(true);
-	});
+describe("SHIP", () => {
+  test("ship is not sunk", () => {
+    testShip.hit();
+    expect(testShip.isSunk()).toBe(false);
+  });
+  test("ship is sunk", () => {
+    testShip.hit();
+    testShip.hit();
+    testShip.hit();
+    expect(testShip.isSunk()).toBe(true);
+  });
 });
 
-describe('GAMEBOARD', () => {
-	const testBoard = gameBoard();
-	testBoard.placeShip(shipName, 'horizontal', 2, 2);
-	test('can place a ship in specific coordinates', () => {
-		expect(testBoard.showArray(2, 2)).not.toBe("");
-	});
-	test('cannot place a ship over the board', () => {
-		expect(testBoard.showArray(6, 5)).toBe("")
+describe("GAMEBOARD", () => {
+  const testBoard = gameBoard();
+  test("can place a ship in specific coordinates", () => {
+    expect(testBoard.placeShip(shipName, "horizontal", 2, 2)).toBe("placed");
+  });
+  test("cannot place a ship outside the board", () => {
+    expect(testBoard.placeShip(shipName, "horizontal", 8, 5)).toBe("ship does not fit");
+  });
+  test("cannot place a ship over another ship coordinates", () => {
+		testBoard.placeShip(shipName, "horizontal", 4, 2)
+		expect(testBoard.placeShip(shipName, "horizontal", 2, 2)).toBe("place is not empty")
 	})
-	test.todo('cannot place a ship over another ship coordinates')
-	test('can hit a ship with specific coordinates', () => {
-		expect(testBoard.receiveAttack(2, 2)).toBe("damaged")
-	})
-	test('can miss a hit', () => {
-		expect(testBoard.receiveAttack(5, 5)).toBe(1)
-	})
+  test("can hit a ship with specific coordinates", () => {
+    expect(testBoard.receiveAttack(2, 2)).toBe("hit");
+  });
+  test("can miss a hit", () => {
+    expect(testBoard.receiveAttack(5, 5)).toBe(1);
+  });
 });
