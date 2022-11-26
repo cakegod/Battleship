f72a8374f25a97c6d5d328d2b04e170dc5d05015
Cake <cakemancerr@gmail.com>
2022-11-26 18:19:29 +0100

    chore: config

--- webpack.config.js
+++ webpack.config.js
@@ -1,23 +1,32 @@
-const HtmlWebpackPlugin = require('html-webpack-plugin')
-const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
 
 module.exports = {
-  mode: 'development',
-  module: {
-    rules: [
-      {
-        test: /\.css$/,
-        use: ['style-loader', 'css-loader']
-      }
-    ]
-  },
-  devtool: 'inline-source-map',
-  devServer: {
-    static: './dist',
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: path.resolve(__dirname, 'src', 'index.html')
-    })
-  ]
-}
+	mode: 'development',
+	entry: './src/index.ts',
+	module: {
+		rules: [
+			{
+				test: /\.css$/,
+				use: ['style-loader', 'css-loader'],
+			},
+			{
+				test: /\.tsx?$/,
+				use: 'ts-loader',
+				exclude: /node_modules/,
+			},
+		],
+	},
+	devtool: 'inline-source-map',
+	devServer: {
+		static: './dist',
+	},
+	plugins: [
+		new HtmlWebpackPlugin({
+			template: path.resolve(__dirname, 'src', 'index.html'),
+		}),
+	],
+	resolve: {
+		extensions: ['.tsx', '.ts', '.js'],
+	},
+};
