efea4abf3426797e7f0f8ff4c49c3a8a8bfe9722
Cake <cakemancerr@gmail.com>
2022-11-26 18:20:45 +0100

    feat: refactor player class

---
+++ src/modules/player.ts
@@ -0,0 +1,67 @@
+import Game from './game';
+import Gameboard from './gameboard';
+import Ship from './ship';
+import { Coordinates } from './types';
+
+class Player {
+	#enemyBoard: Gameboard;
+	#playerBoard: Gameboard;
+	shipFleet: Ship[];
+	direction: 'horizontal' = "horizontal";
+
+	constructor(enemyBoard: Gameboard, playerBoard: Gameboard) {
+		this.#enemyBoard = enemyBoard;
+		this.#playerBoard = playerBoard;
+		this.shipFleet = [
+			new Ship('carrier', 5),
+			new Ship('battleship', 4),
+			new Ship('destroyer', 3),
+			new Ship('submarine', 3),
+			new Ship('patrol boat', 2),
+		];
+	}
+	attack(x: Coordinates, y: Coordinates) {
+		return this.#enemyBoard.receiveAttack(x, y);
+	}
+
+	placeShip(x: Coordinates, y: Coordinates) {
+		console.log(this);
+		
+		if (this.shipFleet.length === 0) return;
+		const placementResult = this.#playerBoard.placeShip(
+			this.shipFleet[0],
+			this.direction,
+			x,
+			y,
+		);
+		if (placementResult === 'has been placed') {
+			this.shipFleet.shift();
+			console.log("test");
+			
+			return 'placed';
+		}
+	}
+}
+
+class ComputerPlayer extends Player {
+	constructor(enemyBoard: Gameboard, playerBoard: Gameboard) {
+		super(enemyBoard, playerBoard);
+	}
+
+	#randomCoordinates() {
+		return Math.floor(Math.random() * 10) as Coordinates;
+	}
+
+	attack() {
+		return super.attack(this.#randomCoordinates(), this.#randomCoordinates());
+	}
+
+	placeShip() {
+		return super.placeShip(
+			this.#randomCoordinates(),
+			this.#randomCoordinates(),
+		);
+	}
+}
+
+export { Player, ComputerPlayer };
