4763d7e068318755a94e92e0b8835ffadffd3548
Cake <cakemancerr@gmail.com>
2022-12-05 22:06:22 +0100

    refactor: to static methods

--- src/modules/gameboard.ts
+++ src/modules/gameboard.ts
@@ -1,18 +1,24 @@
+import Render from './render';
 import Ship from './ship';
 import { Coordinates, Direction } from './types';
+
+type Subscriber = typeof Render['renderAttack' | 'renderShip'];
 
 export default class Gameboard {
 	#board: Ship[][] | ''[][] | 'x'[][] = Array(10)
 		.fill('')
-		.map((_) => Array(10).fill(''));
+		.map(() => Array(10).fill(''));
 	#placedShips: Ship[] = [];
-	#observers: { [key: string]: Function[] } = {};
+	#observers: {
+		[key: string]: Subscriber[];
+	} = {};
 	player: 'computer' | 'human';
+
 	constructor(player: 'computer' | 'human') {
 		this.player = player;
 	}
 
-	subscribe(topic: keyof Gameboard, subscriber: Function) {
+	subscribe(topic: keyof Gameboard, subscriber: Subscriber) {
 		if (!this.#observers[topic]) {
 			this.#observers[topic] = [];
 		}
@@ -20,18 +26,18 @@ export default class Gameboard {
 		this.#observers[topic].push(subscriber);
 	}
 
-	unsubscribe(topic: keyof Gameboard, subscriber: Function) {
+	unsubscribe(topic: keyof Gameboard, subscriber: Subscriber) {
 		this.#observers[topic] = this.#observers[topic].filter(
 			(func) => func !== subscriber,
 		);
 	}
 
 	notify(
-		topic: string,
+		topic: keyof Gameboard,
 		x: Coordinates,
 		y: Coordinates,
-		type?: 'miss' | 'hit',
-		player?: 'computer' | 'human',
+		type: 'miss' | 'hit' = "hit",
+		player: 'computer' | 'human' = this.player,
 	) {
 		if (!this.#observers[topic]) return;
 		this.#observers[topic].forEach((sub) => sub(x, y, type, player));
@@ -41,12 +47,14 @@ export default class Gameboard {
 		const shipLength = ship.getLength();
 
 		// Check if the ship can fit on the gameboard at the specified position
-		if (!this.canFitOnBoard(direction, x, y, shipLength)) {
+		if (!Gameboard.#canFitOnBoard(direction, x, y, shipLength)) {
 			return { success: false, error: 'does not fit board' };
 		}
 
 		// Check if the positions on the gameboard are empty
-		if (!this.arePositionsEmpty(direction, x, y, shipLength)) {
+		if (
+			!Gameboard.#arePositionsEmpty(direction, x, y, shipLength, this.#board)
+		) {
 			return { success: false, error: 'place is occupied' };
 		}
 
@@ -56,13 +64,13 @@ export default class Gameboard {
 			const row = (direction === 'horizontal' ? x + i : x) as Coordinates;
 			const col = (direction === 'horizontal' ? y : y + i) as Coordinates;
 			this.#board[row][col] = ship;
-			this.#observers['placeShip'] && this.notify('placeShip', row, col);
+			this.#observers.placeShip && this.notify('placeShip', row, col);
 		}
 
 		return { success: true };
 	}
 
-	canFitOnBoard(
+	static #canFitOnBoard(
 		direction: Direction,
 		x: Coordinates,
 		y: Coordinates,
@@ -74,11 +82,12 @@ export default class Gameboard {
 		);
 	}
 
-	arePositionsEmpty(
+	static #arePositionsEmpty(
 		direction: Direction,
 		x: Coordinates,
 		y: Coordinates,
 		shipLength: number,
+		board: Ship[][] | ''[][] | 'x'[][],
 	) {
 		// Loop through each position on the gameboard where the ship will be placed
 		for (let i = 0; i < shipLength; i += 1) {
@@ -87,7 +96,7 @@ export default class Gameboard {
 			const col = direction === 'horizontal' ? y : y + i;
 
 			// Check if the position is empty
-			if (this.#board[row][col] !== '') {
+			if (board[row][col] !== '') {
 				return false;
 			}
 		}
